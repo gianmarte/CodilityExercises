0eb5555681f8556adb712af8a798b65c3ab6e00b
gianmarte <42623902+gianmarte@users.noreply.github.com>
2018-09-07 17:11:16 +0800

    Update CodilityFrogJmp.js

--- CodilityFrogJmp.js
+++ CodilityFrogJmp.js
@@ -5,3 +5,5 @@ function solution(X, Y, D) {
     // write your code in JavaScript (Node.js 8.9.4)
     return Math.ceil((Y-X)/D);
 }
+
+//Test Result: https://app.codility.com/demo/results/trainingPU38P5-VUY/?showingAll=1
