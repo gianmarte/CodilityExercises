f8b0ecdc7e805b353894f12e3dd3dafde0adbdd9
gianmarte <42623902+gianmarte@users.noreply.github.com>
2018-09-07 11:51:21 +0800

    Update CodilityMissingInteger.js

--- CodilityMissingInteger.js
+++ CodilityMissingInteger.js
@@ -5,7 +5,6 @@ function solution(A) {
     // write your code in JavaScript (Node.js 8.9.4)
     A.sort((a,b) => a-b);
     let count = 1;
-    let counter = [];
     
     for(let i = 0; i<A.length; i++) {
         if(A[i] > 0){
