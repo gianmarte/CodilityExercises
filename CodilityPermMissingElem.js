841165b89652555aaa747ede119388a75da42f5b
gianmarte <42623902+gianmarte@users.noreply.github.com>
2018-09-07 12:05:35 +0800

    Add files via upload

---
+++ CodilityPermMissingElem.js
@@ -0,0 +1,20 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    A.sort((a,b) => a-b);
+    
+    let count = 1;
+    
+    for(let x = 0; x < A.length; x++){
+        
+        if(A[x] != count){
+            return count;
+        }
+        
+        count++;
+    }
+    
+    return count;
+}
