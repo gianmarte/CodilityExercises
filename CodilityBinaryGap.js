b5a058e5313e45879e61a16b36ac92d052f244bf
gianmarte <42623902+gianmarte@users.noreply.github.com>
2018-09-07 12:56:56 +0800

    Add files via upload

---
+++ CodilityBinaryGap.js
@@ -0,0 +1,26 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    
+    let binary = N.toString(2);
+    let curGap = 0;
+    let count = 0;
+    let indexOne = binary.indexOf(1);
+
+    for(let x = 0; x < binary.length; x++){
+        
+        if(binary.charAt(x) == 0 && indexOne <= x){
+            count++;
+        }
+        else if(binary.charAt(x) == 1 && x > indexOne){
+            if(count > curGap){
+                curGap = count;
+            }
+            count = 0;
+        }
+        
+    }
+    return curGap;
+}
