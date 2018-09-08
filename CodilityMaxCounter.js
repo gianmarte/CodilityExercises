73d03563cc81230b845512eb563c727a6aba4259
gianmarte <42623902+gianmarte@users.noreply.github.com>
2018-09-08 10:32:35 +0800

    Add files via upload

---
+++ CodilityMaxCounter.js
@@ -0,0 +1,31 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(N, A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let arrCount = [];
+    let maxCount = 0;
+    
+    for(let z = N; z > 0; z--){
+        arrCount.push(0);
+    }
+    
+    
+    for(let x = 0; x < A.length; x++){
+        let index = A[x]-1;
+        
+        if(A[x] < N){
+            arrCount[index]+=1;
+        }
+        else{
+            maxCount = Math.max(...arrCount);
+            arrCount = arrCount.map(x => x < 2 ? x=maxCount : x);
+        }
+    }
+    
+    return arrCount;
+    
+}
+
+//Needs improvement
+//Test Result: https://app.codility.com/demo/results/trainingN6NJVT-7QE/?showingAll=1
